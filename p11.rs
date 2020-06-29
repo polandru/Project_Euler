9b8b1a13b1810573e6a3a4ad0cd28f2a464b591b
polandru <paul.andru.p@gmail.com>
2020-06-29 13:01:56 -0500

    Finished Problem 11

--- p11.rs
+++ p11.rs
@@ -1,14 +1,14 @@
+use std::fs;
 
 fn main(){
 
     let numbers = read_file();
     let mut largest = 0;
-
     //Horizontal
   
     for row in 0..20 {
-        for col in 0..16 {
-            let num = numbers[row][col] + numbers[row][col+1] + numbers[row][col+2] + numbers[row][col+3];
+        for col in 0..=16 {
+            let num = numbers[row][col] * numbers[row][col+1] * numbers[row][col+2] * numbers[row][col+3];
             if  num > largest {
                 largest = num;
             }
@@ -18,8 +18,8 @@ fn main(){
 
     //Vertical
     for col in 0..20 {
-        for row in 0..16 {
-            let num = numbers[row][col] + numbers[row+1][col] + numbers[row+2][col] + numbers[row+3][col];
+        for row in 0..=16 {
+            let num = numbers[row][col] * numbers[row+1][col] * numbers[row+2][col] * numbers[row+3][col];
             if num > largest{
                 largest = num;
             }
@@ -27,13 +27,55 @@ fn main(){
     }
 
     //Diagonal
-    
+    for col in (1..=16).rev(){
 
+        for row in 0..=(16-col){
+            let num = numbers[row][col+row] * numbers[row+1][col+1+row] * numbers[row+2][col+2+row] * numbers[row+3][col+3+row];
+            if num > largest{
+                largest = num;
+            }
+        }
+    }
 
+    for row in 0..=16 {
+        for col in 0..=(16 - row) {
+            let num = numbers[row+col][col] * numbers[row+1+col][col+1] * numbers[row+2+col][col+2] * numbers[row+3+col][col+3];
+            if num > largest{
+                largest = num;
+            }
+        } 
+    }
+
+    for col in 3..20{
+        for row in 0..=(col - 3){
+            let num = numbers[row][col-row] * numbers[row+1][col-1-row] * numbers[row+2][col-2-row] * numbers[row+3][col-3-row];
+            if num > largest{
+                largest = num;
+            }
+        }
+    }
+  
+    for row in (1..=16).rev() {
+        for col in (row+3)..=19 {
+            let num = numbers[row+19-col][col] * numbers[row+20-col][col-1] * numbers[row+21-col][col-2] * numbers[row+22-col][col-3];
+            if num > largest{
+                largest = num;
+            }
+        } 
+    }
+    print!("Largest {}\n", largest);
 }
 
-fn read_file() -> [[i32;20];20] {
-    let numbers = [[0;20]; 20];
-
+fn read_file() -> [[isize;20];20] {
+    let mut numbers = [[0;20]; 20];
+    let file_contents = fs::read_to_string("p11.data").expect("Unable to read");
+    
+    let mut i = 0;
+    for row in 0..20{
+        for col in 0..20 {        
+            numbers[row][col] = file_contents[i..(i+2)].parse().unwrap();
+            i += 3;
+        }
+    }
     numbers
 }
